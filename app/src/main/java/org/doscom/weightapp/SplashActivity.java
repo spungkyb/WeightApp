6964b1b1976944ca97efe2e18fdbdb727449704c
spungkyb <sevianapugnykb@gmail.com>
2018-03-15 00:50:46 +0700

    Initial commit

---
+++ app/src/main/java/org/doscom/weightapp/SplashActivity.java
@@ -0,0 +1,31 @@
+package org.doscom.weightapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+
+        //menghilangkan ActionBar
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+
+}
